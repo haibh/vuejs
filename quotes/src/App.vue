99a559f86e20d417d5ae926557ed63170b12614b
haibh <haibhut@gmail.com>
2017-11-24 15:54:08 +0700

    slot for quote

---
+++ quotes/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <app-quote>
+          <h2 slot="title"> {{ quoteTitle }}</h2>
+          <p slot="content">Wonderful quote</p>
+        </app-quote>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Quote from "./components/Quote.vue";
+
+export default {
+  data: function(){
+    return {
+      quoteTitle: 'The Quote'
+    }
+  },
+  components: {
+    appQuote: Quote
+  }
+};
+</script>
+
+<style>
+
+</style>
