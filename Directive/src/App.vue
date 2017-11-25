b7eeaed988ea797c9104b83c63574bb327066e34
haibhut@gmail.com <12345678?a>
2017-11-26 00:06:36 +0700

    Filters & Mixins first Initial

--- Directive/src/App.vue
+++ Directive/src/App.vue
@@ -19,42 +19,45 @@
 </template>
 
 <script>
-    export default {
-        directives: {
-            'local-highlight': {
-                bind(el, binding, vnode) {
-                    var delay = 0;
-                    if (binding.modifiers['delayed']) {
-                        delay = 3000;
-                    }
-                    if (binding.modifiers['blink']) {
-                        let mainColor = binding.value.mainColor;
-                        let secondColor = binding.value.secondColor;
-                        let currentColor = mainColor;
-                        setTimeout(() => {
-                            setInterval(() => {
-                                currentColor == secondColor ? currentColor = mainColor : currentColor = secondColor;
-                                if (binding.arg == 'background') {
-                                    el.style.backgroundColor = currentColor;
-                                } else {
-                                    el.style.color = currentColor;
-                                }
-                            }, binding.value.delay);
-                        }, delay);
-                    } else {
-                        setTimeout(() => {
-                            if (binding.arg == 'background') {
-                                el.style.backgroundColor = binding.value.mainColor;
-                            } else {
-                                el.style.color = binding.value.mainColor;
-                            }
-                        }, delay);
-                    }
-                }
-            }
+export default {
+  directives: {
+    "local-highlight": {
+      bind(el, binding, vnode) {
+        var delay = 0;
+        if (binding.modifiers["delayed"]) {
+          delay = 3000;
         }
+        if (binding.modifiers["blink"]) {
+          let mainColor = binding.value.mainColor;
+          let secondColor = binding.value.secondColor;
+          let currentColor = mainColor;
+          setTimeout(() => {
+            setInterval(() => {
+              currentColor == secondColor
+                ? (currentColor = mainColor)
+                : (currentColor = secondColor);
+              if (binding.arg == "background") {
+                el.style.backgroundColor = currentColor;
+              } else {
+                el.style.color = currentColor;
+              }
+            }, binding.value.delay);
+          }, delay);
+        } else {
+          setTimeout(() => {
+            if (binding.arg == "background") {
+              el.style.backgroundColor = binding.value.mainColor;
+            } else {
+              el.style.color = binding.value.mainColor;
+            }
+          }, delay);
+        }
+      }
     }
+  }
+};
 </script>
 
 <style>
+
 </style>
