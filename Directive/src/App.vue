0fd91eadbbc12d99e7196335238b78084257c5c4
haibhut@gmail.com <12345678?a>
2017-11-25 23:51:10 +0700

    update directive

--- Directive/src/App.vue
+++ Directive/src/App.vue
@@ -12,9 +12,8 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom Directives</h1>
-                <p v-text="'asd'"></p>
-                <p v-html="'<strong>Strong</strong>'"></p>
-
+                <p v-highlight:background.delayed="'red'"> Color this</p>
+                <p v-local-highlight:background.delayed.blink="'blue'"> Color this, again</p>
             </div>
         </div>
 
@@ -22,7 +21,43 @@
 </template>
 
 <script>
-export default {};
+export default {
+  directives: {
+    "local-highlight": {
+      bind(el, binding, vnode) {
+        var delay = 0;
+        if (binding.modifiers["delayed"]) {
+          delay = 3000;
+        }
+        if (binding.modifiers["blink"]) {
+          let mainColor = binding.value;
+          let secondColor = "black";
+          let currentColor = mainColor;
+          setTimeout(() => {
+            setInterval(() => {
+              currentColor == secondColor
+                ? (currentColor = mainColor)
+                : (currentColor = secondColor);
+              if (binding.arg == "background") {
+                el.style.backgroundColor = currentColor;
+              } else {
+                el.style.color = currentColor;
+              }
+            }, 1000);
+          }, delay);
+        } else {
+          setTimeout(() => {
+            if (binding.arg == "background") {
+              el.style.backgroundColor = binding.value;
+            } else {
+              el.style.color = binding.value;
+            }
+          }, delay);
+        }
+      }
+    }
+  }
+};
 </script>
 
 <style>
