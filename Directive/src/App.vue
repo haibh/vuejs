28c450a7cd22337b26f9b993363bbe543c251bbc
haibhut@gmail.com <12345678?a>
2017-11-25 11:55:22 +0700

    Update vue-paper-dashboard-pro-v1.1.0

--- Directive/src/App.vue
+++ Directive/src/App.vue
@@ -3,15 +3,26 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Directives</h1>
+                <p v-text="'Hello'"></p>
+                <p v-html="'<strong>Strong</strong>'"></p>
 
             </div>
         </div>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Custom Directives</h1>
+                <p v-text="'asd'"></p>
+                <p v-html="'<strong>Strong</strong>'"></p>
+
+            </div>
+        </div>
+
     </div>
 </template>
 
 <script>
-    export default {
-    }
+export default {};
 </script>
 
 <style>
