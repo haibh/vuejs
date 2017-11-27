4f40aba66ebb9a92a54921ff6acc49874c8a4813
haibh <haibhut@gmail.com>
2017-11-27 17:40:25 +0700

    routing first initial

--- vue-resource/src/App.vue
+++ vue-resource/src/App.vue
@@ -14,11 +14,13 @@
                 <button class="btn btn-primary" @click="submit">Submit</button>
                 <hr>
                 <input class="form-control" type="text" v-model="node">
-                <br><br>
+                <br>
+                <br>
                 <button class="btn btn-primary" @click="fetchData">Get Data</button>
-                <br><br>
+                <br>
+                <br>
                 <ul class="list-group">
-                    <li class="list-group-item" v-for="u in users">{{ u.username }} - {{ u.email }}</li>
+                    <li class="list-group-item" v-for="u in users" :key="u">{{ u.username }} - {{ u.email }}</li>
                 </ul>
             </div>
         </div>
@@ -40,44 +42,51 @@
         },
         methods: {
             submit() {
-//                this.$http.post('data.json', this.user)
-//                        .then(response => {
-//                            console.log(response);
-//                        }, error => {
-//                            console.log(error);
-//                        });
-//                this.resource.save({}, this.user);
+                //                this.$http.post('data.json', this.user)
+                //                        .then(response => {
+                //                            console.log(response);
+                //                        }, error => {
+                //                            console.log(error);
+                //                        });
+                //                this.resource.save({}, this.user);
                 this.resource.saveAlt(this.user);
             },
             fetchData() {
-//                this.$http.get('data.json')
-//                        .then(response => {
-//                            return response.json();
-//                        })
-//                        .then(data => {
-//                            const resultArray = [];
-//                            for (let key in data) {
-//                                resultArray.push(data[key]);
-//                            }
-//                            this.users = resultArray;
-//                        });
-                this.resource.getData({node: this.node})
-                        .then(response => {
-                            return response.json();
-                        })
-                        .then(data => {
-                            const resultArray = [];
-                            for (let key in data) {
-                                resultArray.push(data[key]);
-                            }
-                            this.users = resultArray;
-                        });
+                //                this.$http.get('data.json')
+                //                        .then(response => {
+                //                            return response.json();
+                //                        })
+                //                        .then(data => {
+                //                            const resultArray = [];
+                //                            for (let key in data) {
+                //                                resultArray.push(data[key]);
+                //                            }
+                //                            this.users = resultArray;
+                //                        });
+                this.resource.getData({
+                        node: this.node
+                    })
+                    .then(response => {
+                        return response.json();
+                    })
+                    .then(data => {
+                        const resultArray = [];
+                        for (let key in data) {
+                            resultArray.push(data[key]);
+                        }
+                        this.users = resultArray;
+                    });
             }
         },
         created() {
             const customActions = {
-                saveAlt: {method: 'POST', url: 'alternative.json'},
-                getData: {method: 'GET'}
+                saveAlt: {
+                    method: 'POST',
+                    url: 'alternative.json'
+                },
+                getData: {
+                    method: 'GET'
+                }
             };
             this.resource = this.$resource('{node}.json', {}, customActions);
         }
@@ -85,4 +94,5 @@
 </script>
 
 <style>
+
 </style>
--- vue-resource/src/main.js
+++ vue-resource/src/main.js
@@ -6,16 +6,20 @@ Vue.use(VueResource);
 
 Vue.http.options.root = 'https://vuejs-http.firebaseio.com/';
 Vue.http.interceptors.push((request, next) => {
-  console.log(request);
-  if (request.method == 'POST') {
-    request.method = 'PUT';
-  }
-  next(response => {
-    response.json = () => { return {messages: response.body} }
-  });
+    console.log(request);
+    if (request.method == 'POST') {
+        request.method = 'PUT';
+    }
+    next(response => {
+        response.json = () => {
+            return {
+                messages: response.body
+            }
+        }
+    });
 });
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    render: h => h(App)
 })
