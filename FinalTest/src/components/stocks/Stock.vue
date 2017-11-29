7a3ce2fcb2a6c2cbd10b7636c4f51892c6febbec
haibhut@gmail.com <12345678?a>
2017-11-29 23:03:08 +0700

    Update finalTest

--- FinalTest/src/components/stocks/Stock.vue
+++ FinalTest/src/components/stocks/Stock.vue
@@ -2,24 +2,39 @@
     <div class="col-sm-6 col-md-4">
         <div class="panel panel-success">
             <div class="panel-heading">
-                <h3 class="panel-title"></h3>
-                {{stock.name}}
-                <small>(Price: {{stock.price}})</small>
+                <h3 class="panel-title">
+                    {{ stock.name }}
+                    <small>(Price: {{ stock.price }})</small>
                 </h3>
             </div>
             <div class="panel-body">
                 <div class="pull-left">
-                    <input type="number" class="form-control" placeholder="Quantity" v-model="quantity">
+                    <input
+                            type="number"
+                            class="form-control"
+                            placeholder="Quantity"
+                            v-model="quantity"
+                            :class="{danger: insufficientFunds}"
+                    >
                 </div>
                 <div class="pull-right">
-                    <button class="btn btn-success" @click="buyStock" :disabled="quantity <=0">
-                        Buy
+                    <button
+                            class="btn btn-success"
+                            @click="buyStock"
+                            :disabled="insufficientFunds || quantity <= 0 || !Number.isInteger(quantity)"
+                    >{{ insufficientFunds ? 'Insufficient Funds' : 'Buy' }}
                     </button>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
+<style scoped>
+    .danger {
+        border: 1px solid red;
+    }
+</style>
 
 <script>
     export default {
@@ -29,6 +44,14 @@
                 quantity: 0
             }
         },
+        computed: {
+            funds() {
+                return this.$store.getters.funds;
+            },
+            insufficientFunds() {
+                return this.quantity * this.stock.price > this.funds;
+            }
+        },
         methods: {
             buyStock() {
                 const order = {
@@ -36,13 +59,9 @@
                     stockPrice: this.stock.price,
                     quantity: this.quantity
                 };
-                console.log(order);
+                this.$store.dispatch('buyStock', order);
                 this.quantity = 0;
             }
         }
     }
 </script>
-
-<style>
-
-</style>
