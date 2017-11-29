8ec24d1b7cc314fd6da67c831f4401441c291bd5
haibh <haibhut@gmail.com>
2017-11-29 18:20:36 +0700

    update final test

--- FinalTest/src/components/stocks/Stocks.vue
+++ FinalTest/src/components/stocks/Stocks.vue
@@ -1,5 +1,39 @@
 <template>
   <div>
-    <h1>The stocks coponents</h1>
+    <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
   </div>
 </template>
+
+<script>
+    import Stock from './Stock.vue'
+    export default {
+        data() {
+            return {
+                stocks: [{
+                        id: 1,
+                        name: 'BMW',
+                        price: 110
+                    },
+                    {
+                        id: 2,
+                        name: 'Google',
+                        price: 200
+                    },
+                    {
+                        id: 1,
+                        name: 'Apple',
+                        price: 250
+                    },
+                    {
+                        id: 1,
+                        name: 'Twitter',
+                        price: 8
+                    }
+                ]
+            }
+        },
+        components: {
+            appStock: Stock
+        }
+    }
+</script>
